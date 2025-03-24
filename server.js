a1e4473fb7d13059d79b85752e98300bacf9134d
devangmalhotra <malh2226@mylaurier.ca>
2025-03-24 13:48:34 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,130 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const puppeteer = require('puppeteer');
+const mysql = require('mysql');
+
+app.use(cors());
+
+//Create connection with mysql database (id, title, price, location, isFurnished, link)
+const con = mysql.createConnection({
+    host: 'sql12.freesqldatabase.com', 
+    user: 'sql12768879', 
+    password: 'jxEhANMr7a'
+});
+
+
+app.get("/scrape", (req, res) => {
+    const cityJson = req.query;
+    const cityToScrape = cityJson.city;
+    console.log(cityToScrape);
+    res.send({hello: 'hello world'});
+    
+    const scrapeObj = new Scrape(cityToScrape);
+    scrapeObj.initialize();
+    
+});
+
+app.get("/toronto", (req, res) => {
+    res.send({hello: 'hello world'});
+});
+
+app.get("/hamilton", (req, res) => {
+    res.send({hello: 'hello world'});
+});
+
+app.listen(5000, () => {
+    console.log("Listening on port 5000...");
+});
+
+class Scrape {
+    constructor(city) {
+        this.city = city;
+        this.browser = null;
+        this.page = null;
+    }
+
+    async initialize() {
+        this.browser = await puppeteer.launch({headless: false});
+        this.page = await this.browser.newPage();
+        await this.page.setViewport({width: 1280, height: 800});
+        //await this.page.goto("https://pptr.dev/guides/headless-modes");
+        //await this.browser.close();
+        eval(`this.${this.city}Scrape()`);
+    }
+    
+    async waterlooScrape() {
+        let linksArr = [];
+        console.log("waterlooScrape"); 
+
+        //Kijiji
+        await this.page.goto("https://www.kijiji.ca/b-canada/student-housing-waterloo/k0l0?dc=true&view=list");
+        const adsResultsDiv = await this.page.$('[data-testid=srp-search-list]');
+        const postingsList = await adsResultsDiv.$$("li");
+        //Collecting all links
+        for (const i of postingsList) {
+            try {
+                const anchorElement = await i.$('[data-testid=listing-link]');
+                const adLink = await this.page.evaluate(el => el.getAttribute('href'), anchorElement);
+                linksArr.push(adLink);
+            }
+            catch(e) {
+                
+            }
+        }
+        //Accessing all links
+        for (const a of linksArr) {
+            try {
+                await this.page.goto(a);
+                const titleElement = await this.page.$('.sc-9d9a3b6-0.iacITa')
+                const adTitle = (await titleElement.evaluate(el => el.innerHTML)).trim();
+                const priceElement = await this.page.$('[data-testid=vip-price]');
+                const adPrice = (await priceElement.evaluate(el => el.innerHTML)).trim().replace(",", "").replace("$", "");
+                const locationElement = await this.page.$('::-p-xpath(//*[@id="base-layout-main-wrapper"]/div[1]/div[2]/section/div/div/div[1]/div/div/div[2]/div/div/button)');
+                const adLocation = (await locationElement.evaluate(el => el.innerHTML)).trim();
+                let adIsFurnished;
+                const isFurnishedCandidates = await this.page.$$('.sc-eb45309b-0.iNzWBi');
+                try {
+                    for (const i of isFurnishedCandidates) {
+                        const candidateContainerElement = await i.$('.sc-9d9a3b6-0.fAJNmV');
+                        const candidateTitle = (await candidateContainerElement.evaluate(el => el.innerHTML)).trim();
+                        if (candidateTitle == "Furnished") {
+                            const isFurnishedElement = await i.$('.sc-578235a5-0.hsLMPB');
+                            adIsFurnished = await isFurnishedElement.evaluate(el => el.innerHTML);
+                            console.log(adIsFurnished);
+                        }
+                    }
+                }
+                catch(e) {
+                    //console.log(e);
+                }
+                
+
+                console.log(adTitle);
+                console.log(adPrice);
+                console.log(adLocation);
+                console.log(adIsFurnished);
+                console.log("");
+            }
+            catch (e) {
+                //console.log(e);
+                //break;
+            }
+        }
+
+        //console.log(linksArr);
+
+        //const price = await postingsList[0].$('[data-testid=listing-price]')
+        //console.log(await price.evaluate(el => el.innerHTML, price));
+        await this.browser.close()
+    }
+
+    torontoScrape() {
+        console.log("torontoScrape");
+    }
+
+    hamiltonScrape() {
+        console.log("hamiltonScrape");
+    }
+}
+

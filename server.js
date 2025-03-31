a6473bde5b4f89a435ccb916ab3b4d584fcca269
devangmalhotra <malh2226@mylaurier.ca>
2025-03-31 11:15:16 -0400

    'Updated files'

--- server.js
+++ server.js
@@ -193,7 +193,7 @@ class Scrape {
         console.log("Finished scraping Waterloo Data...");
     }
 
-    async torontoScrape() {
+    async torontoScrape() {  //Kijiji, Places4Students
         console.log("Scraping Toronto Data...");
         await this.getKijijiInfo("https://www.kijiji.ca/b-canada/student-housing-toronto/k0l0?dc=true&view=list");
         await this.getPlaces4StudentsInfo("https://www.places4students.com/Places/PropertyListings?SchoolID=FzhtQRDGtSU%3d"); // University of Toronto
